d28cc6b7292a1d11be52279e19c6690c97d9aea5
Pascal Zöller <pascal.zoeller@itc-studenten.de>
2021-04-07 10:30:11 +0200

    button opens change map activity

--- app/src/main/java/com/example/historicmapsdo/MapsActivity.kt
+++ app/src/main/java/com/example/historicmapsdo/MapsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.historicmapsdo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -39,5 +42,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Add a marker in Sydney and move the camera
         mMap.addMarker(MarkerOptions().position(dortmund).title("Marker in Dortmund"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dortmund, 15f))
+    }
+
+    fun openChangeMap(view: View){
+        startActivity(Intent(this, changeMap::class.java))
     }
 }
