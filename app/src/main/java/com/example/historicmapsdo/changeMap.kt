bb54529d6bff51e27d60979f94cdf304fd94d227
Pascal Zöller <pascal.zoeller@itc-studenten.de>
2021-04-07 10:05:12 +0200

    added change map activity

---
+++ app/src/main/java/com/example/historicmapsdo/changeMap.kt
@@ -0,0 +1,11 @@
+package com.example.historicmapsdo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class changeMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_map)
+    }
+}
